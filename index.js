208dd028b728deab211e4840d599b4618a655142
norwegian-octopus <orekhovsky.aa@gmail.com>
2022-10-13 13:30:32 +0300

    Reworked classes, add sprites of background, shop

--- index.js
+++ index.js
@@ -6,67 +6,26 @@ canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imgSrc: './assets/background.png'
+});
 
-class Sprite {
-    constructor({ position, velocity, color = 'red', offset }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.height = 150;
-        this.width = 50;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        }
-        this.color = color;
-        this.isAttacking;
-        this.health = 100;
-    }
+const shop = new Sprite({
+    position: {
+        x: 594,
+        y: 128
+    },
+    imgSrc: './assets/shop.png',
+    scale: 2.75,
+    frames: 6
+});
 
-    draw() {
-        // player - enemy
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
 
-        // attack box
-        if (this.isAttacking) {
-            c.fillStyle = 'green';
-            c.fillRect(
-                this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height
-            );
-        }
-    }
-
-    update() {
-        this.draw();
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0;
-        } else this.velocity.y += gravity;
-    }
-
-    attack() {
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -82,7 +41,7 @@ const player = new Sprite({
 });
 
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
@@ -113,39 +72,7 @@ const keys = {
     }
 }
 
-function rectCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-        rectangle1.attackBox.position.y < + rectangle2.position.y + rectangle2.height
-    );
-}
 
-function determineWinner({player, enemy, timerId}) {
-    clearTimeout(timerId);
-    document.querySelector('#fighting-result').style.display = 'flex';
-    if (player.health === enemy.health) {
-        document.querySelector('#fighting-result').innerHTML = 'TIE';
-    } else if (player.health > enemy.health) {
-        document.querySelector('#fighting-result').innerHTML = 'Player 1 Wins';
-    } else if (player.health < enemy.health) {
-        document.querySelector('#fighting-result').innerHTML = 'Player 2 Wins';
-    }
-}
-
-let timer = 60;
-let timerId;
-function decreaseTimer() {
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000);
-        timer--;
-        document.querySelector('#timer').innerHTML = timer;
-    }
-    if (timer === 0) {
-        determineWinner({player, enemy, timerId});
-    }
-}
 
 decreaseTimer();
 
@@ -153,6 +80,8 @@ function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
+    background.update();
+    shop.update();
     player.update();
     enemy.update();
 
@@ -200,7 +129,7 @@ function animate() {
 
     // end game conditions
     if (enemy.health <= 0 || player.health <= 0) {
-        determineWinner({player, enemy, timerId});
+        determineWinner({ player, enemy, timerId });
     }
 }
 
